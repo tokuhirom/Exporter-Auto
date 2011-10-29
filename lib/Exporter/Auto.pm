7196e5578f0c1203e5609e1c978b3ee17bef937d
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-10-29 11:46:42 +0900

    initial implement

--- lib/Exporter/Auto.pm
+++ lib/Exporter/Auto.pm
@@ -4,7 +4,26 @@ use warnings;
 use 5.008008;
 our $VERSION = '0.01';
 
+use Sub::Identify qw(stash_name);
+use B::Hooks::EndOfScope;
+use Exporter;
 
+sub import {
+    my $klass = caller(0);
+
+    no strict 'refs';
+    unshift @{"${klass}::ISA"}, 'Exporter';
+
+    on_scope_end {
+        while (my ($k, $v) = each %{"${klass}::"}) {
+            next if $k =~ /^(?:BEGIN|CHECK|END)$/;
+            next if $k =~ /^_/;
+            next unless *{"${klass}::${k}"}{CODE};
+            next if $klass ne stash_name($klass->can($k));
+            push @{"${klass}::EXPORT"}, $k;
+        }
+    };
+}
 
 1;
 __END__
@@ -13,15 +32,15 @@ __END__
 
 =head1 NAME
 
-Exporter::Auto -
+Exporter::Auto - Export public functions automatically.
 
 =head1 SYNOPSIS
 
-  use Exporter::Auto;
+    use Exporter::Auto;
 
 =head1 DESCRIPTION
 
-Exporter::Auto is
+I'm tired to push function names to @EXPORT. Perl5 should add functions automatically!!!111
 
 =head1 AUTHOR
 
