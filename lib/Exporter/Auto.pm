076e8639de253e711adfb8784544366147716ab1
Neil Bowers <neil@bowers.com>
2014-08-16 19:34:41 +0100

    Tweaked abstract and added DESCRIPTION section

--- lib/Exporter/Auto.pm
+++ lib/Exporter/Auto.pm
@@ -2,7 +2,7 @@ package Exporter::Auto;
 use strict;
 use warnings;
 use 5.008005;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Sub::Identify qw(stash_name);
 use B::Hooks::EndOfScope;
@@ -33,7 +33,7 @@ __END__
 
 =head1 NAME
 
-Exporter::Auto - Export public functions automatically.
+Exporter::Auto - export all public functions from your package
 
 =head1 SYNOPSIS
 
@@ -48,7 +48,41 @@ Exporter::Auto - Export public functions automatically.
 
 =head1 DESCRIPTION
 
-I'm tired to push function names to @EXPORT. Perl5 should add functions automatically!!!111
+Exporter::Auto is a simple replacement for L<Exporter> that will export
+all public functions from your package. If you want all functions to be
+exported from your module by default, then this might be the module for you.
+If you only want some functions exported, or want tags, or to export variables,
+then you should look at one of the other Exporter modules (L</"SEE ALSO">).
+
+Let's say you have a library module with three functions, all of which
+you want to export by default. With L<Exporter>, you'd write something like:
+
+    package MyLibrary;
+    use parent 'Exporter';
+    our @EXPORT = qw/ foo bar baz /;
+    sub foo { ... }
+    sub bar { ... }
+    sub baz { ... }
+    1;
+
+Every time you add a new function,
+you must remember to add it to C<@EXPORT>.
+Not a big hassle, but a small inconvenience.
+
+With C<Exporter::Auto> you just write:
+
+    package MyLibrary;
+    use Exporter::Auto;
+    sub foo { ... }
+    sub bar { ... }
+    sub baz { ... }
+    1;
+
+When you C<use Exporter::Auto> it automatically adds an C<import> function
+to your package, so you don't need to declare your package as a subclass.
+
+That's it. If you want anything more fancy than this,
+it's time for another module.
 
 =head1 REPOSITORY
 
